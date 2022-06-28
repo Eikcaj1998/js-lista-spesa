6a6ecdf2f967777cd21e044d9e654d4d24d9f351
Eikcaj.1998 <jw220998@gmail.com>
2022-06-28 15:05:25 +0200

    base

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Rivediamo l’esercizio della lista della spesa visto insieme, 
+ma questa volta con il ciclo while al posto del ciclo for. 
+Facciamo attenzione a non dimenticare tutte le differenze implementative che 
+il ciclo while ci impone, in particolare alla variabile di indice. */
+console.log('JS OK')
+
+
