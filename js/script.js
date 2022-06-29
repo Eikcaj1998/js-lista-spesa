82c226396e293d92c0dd1dad32feba32065a5272
Eikcaj.1998 <jw220998@gmail.com>
2022-06-29 09:08:54 +0200

    es-finito

--- js/script.js
+++ js/script.js
@@ -4,4 +4,25 @@ Facciamo attenzione a non dimenticare tutte le differenze implementative che
 il ciclo while ci impone, in particolare alla variabile di indice. */
 console.log('JS OK')
 
+const listArray= [
+    'Uova',
+    'Pane',
+    'Latte',
+    'Biscotti',
+    'Pasta',
+    'Affettati',
+    'Formaggio',
+    'Verdura',
+    'Frutta',
+];
 
+const shoppingList = document.getElementById('shopping-list');
+let i = 0;
+while (i < listArray.length ){
+    const shoppingListElement = document.createElement('li');
+    shoppingList.append(shoppingListElement);
+    shoppingListElement.innerHTML = listArray[i];
+    i++;
+}
+console.log(listArray)
+    
